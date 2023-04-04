063d4b0ff75822eb6e43c7738e71ea7655c949ee
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-04-04 18:24:01 -0300

    Add do back-end

--- TelaLivros.php
+++ TelaLivros.php
@@ -1,29 +1,26 @@
+<?php
 
-<!--
+if (isset( $_GET['busca'] ) ) {
 
-if (isset( $_POST['nome'] ) ) {
-
-    $nome = $_POST['nome'];
+    $pesquisa = $_GET['busca'];
 
 } else {
 
-   $nome = '';
+   $pesquisa = '';
 
 }
 
 include_once('conexao.php');
 
-/*seleciona a tabela livros dentro do banco de dados biblioteca_bd*/
-$sql = "SELECT *  FROM biblioteca_bd WHERE livros LIKE '%$nome%' ";
+$sql = "SELECT *  FROM livro 
+                WHERE titulo LIKE '%$pesquisa%' 
+                OR autor LIKE '%$pesquisa%'
+                OR categoria LIKE '%$pesquisa%'
+                OR quantidade LIKE
+                  '%$pesquisa%' ";
 
-$dados = mysqli_query($conexao, $sql);
+$dados = mysqli_query($mysqli, $sql);
 
-
-?>
--->
-<?php
-
-include('conexao.php');
 
 ?>
 <!DOCTYPE html>
@@ -47,111 +44,40 @@ include('conexao.php');
       <div class="div-amarelo"></div>
       
       <form class="div-search" action="">
-      <input name="busca" class="input-search" type="search" placeholder="Pesquisar ex: Livro..." value="<?php if(isset($_GET['busca'])) echo $_GET['busca']; ?>" />
+      <input name="busca" class="input-search" type="search" placeholder="Pesquisar ex: Livro..." />
       <button type="submit" class="btn-search"><i class="fa fa-search"></i></button>
       </form>
  
       
-    <h1 class="categoria-title">Romance</h1>
+    <h1 class="categoria-title">Livros</h1>
     
     <div class="livros-list">
      <!-- Os Livros aparecem dentro da livros-list -->
-     <?php
-      if (!isset($_GET['busca'])) {
-        ?>
-        
-        <?php
-        }
-        else {
-            $pesquisa = $mysqli->real_escape_string($_GET['busca']);
-            $sql_code = "SELECT * 
-                FROM livro 
-                WHERE titulo LIKE '%$pesquisa%' 
-                OR autor LIKE '%$pesquisa%'
-                OR categoria LIKE '%$pesquisa%'
-                OR quantidade LIKE
-                  '%$pesquisa%'";
-            $sql_query = $mysqli->query($sql_code) or die("ERRO ao consultar! " . $mysqli->error); 
-            
-              while($dados = $sql_query->fetch_assoc()) {
-                ?>
-               
-               <div class="container-livro">
-                <div class="livro">
-                <p class="livro-titulo"> <?php echo $dados[titulo]; ?></p>
-                <p class="livro-autor"><?php echo $dados[titulo]; ?></p>
-                </div>
-                <div class="span">
-                <p class="disponivel">Disponível</p>
-                <p class="livro-quantidade"><?php echo $dados[titulo]; ?></p>
-                </div>
-              </div>
-             <?php
-            }
-            ?>
-        <?php
-        }
-     ?>
-     
-     
-      <div class="container-livro">
-        <div class="livro">
-          <p class="livro-titulo">Dom Casmurro</p>
-          <p class="livro-autor">Machado de Assis</p>
-        </div>
-        <div class="span">
-          <p class="disponivel">Disponível</p>
-          <p class="livro-quantidade">2</p>
-        </div>
-      </div>
-      
-      <div class="container-livro">
-        <div class="livro">
-          <p class="livro-titulo">A Culpa É das Estrelas</p>
-          <p class="livro-autor">John Green</p>
-        </div>
-        <div class="span">
-          <p class="disponivel">Disponível</p>
-          <p class="livro-quantidade">0</p>
-        </div>
-      </div>
-      
-      <div class="container-livro">
-        <div class="livro">
-          <p class="livro-titulo">Madame Bovary</p>
-          <p class="livro-autor">Gustave Flaubert</p>
-        </div>
-        <div class="span">
-          <p class="disponivel">Disponível</p>
-          <p class="livro-quantidade">1</p>
-        </div>
-      </div>
-      
-      
-      
-      <!--PHP para exibir o conteúdo
-      
+ 
+      <?php
+
           while ($livro = mysqli_fetch_assoc($dados) ) {
           $livroTitulo = $livro['titulo'];
           $livroAutor = $livro['autor'];
           $livroCategoria = $livro['categoria'];
-          $livroQuantidade = $livro['variavel_quantidade_no_BD'];
-          
-          echo "
-          <div class="container-livro">
-          <div class="livro">
-          <p class="livro-titulo"> $livroTitulo </p>
-          <p class="livro-autor"> $livroAutor </p>
+          $livroQuantidade = $livro['quantidade'];
+          ?>
+          <div class='container-livro'>
+          <div class='livro'>
+          <p class='livro-titulo'><?php echo $livroTitulo ?></p>
+          <p class='livro-autor'><?php echo $livroAutor ?></p>
           </div>
-          <div class="span">
-          <p class="disponivel">Disponível</p>
-          <p class="livro-quantidade"> $livroQuantidade </p>
+          <div class='span'>
+          <p class='disponivel'>Disponível</p>
+          <p class='livro-quantidade'><?php echo $livroQuantidade ?></p>
           </div>
           </div>
-          "
+         
+          <?php
           }
+          
       ?>
-      -->
+      
     </div>
     
     <div class="footer">
